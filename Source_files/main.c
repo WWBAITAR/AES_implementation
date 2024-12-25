ebc67e44b2c00dc91f41553a158cc8eca44bedcd
walid baitar <walid.baitar@nxp.com>
2024-12-25 14:15:54 +0100

    Implementation of subBytes and mixColumns operations

--- Source_files/main.c
+++ Source_files/main.c
@@ -3,12 +3,12 @@
 
 int main()
 {
-    printf("Testing shiftRows function \n");
+    printf("Testing mixcolumb function \n");
 
-    uint8_t plaintext[16] = {0x12, 0x34, 0x56, 0x78, 
-                             0x12, 0x34, 0x56, 0x78,
-                             0x12, 0x34, 0x56, 0x78,
-                             0x12, 0x34, 0x56, 0x78};
+    uint8_t plaintext[16] = {0x87, 0xF2, 0x4D, 0x97, 
+                             0x6E, 0x4C, 0x90, 0xEC,
+                             0x46, 0xE7, 0x4A, 0xC3,
+                             0xA6, 0x8C, 0xD8, 0x95};
     
     printf("Our plainText is: \n");
 
@@ -17,14 +17,14 @@ int main()
         printf("%x %x %x %x \n",plaintext[0 + 4*i],plaintext[1 + 4*i],plaintext[2 + 4*i],plaintext[3 + 4*i]);
     }
 
-    shift_rows(plaintext);
+    mix_columns(plaintext);
 
-    printf("Our plainText after shiftRows is: \n");
+    printf("Our plainText after mixColumns is: \n");
 
     for(int i=0; i<4; i++)
     {
         printf("%x %x %x %x \n",plaintext[0 + 4*i],plaintext[1 + 4*i],plaintext[2 + 4*i],plaintext[3 + 4*i]);
-    }
+    }    
 
     return 0;
 }
